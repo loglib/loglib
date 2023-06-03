1dd8e8378f5147810963262bb47a5582055ac805
Bereket Engida <bereket@titantech.duckdns.org>
2023-06-03 23:33:37 +0300

    url dashbaord

--- packages/tracker/src/utils/util.ts
+++ packages/tracker/src/utils/util.ts
@@ -103,9 +103,9 @@ export function getUrl() {
 		return process.env.LOGLIB_URL
 	}
 	if (process.env.VERCEL_URL) {
-		return 'https://' + process.env.VERCEL_URL + '/api/loglib'
+		return '/api/loglib'
 	}
 	if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-		return 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL + '/api/loglib'
+		return '/api/loglib'
 	}
 }
