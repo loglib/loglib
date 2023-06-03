9174b102313d56c0b5e3fb1b2f005d6d5edfbe71
Bereket Engida <bereket@titantech.duckdns.org>
2023-06-03 10:28:47 +0300

    kinda done

--- packages/adapter-supabase/src/tables.sql
+++ packages/adapter-supabase/src/tables.sql
@@ -10,8 +10,8 @@ create table if not exists
 create table if not exists
   public.web_session (
     id text not null,
-    created_at timestamp without time zone not null default current_timestamp,
-    updated_at timestamp without time zone not null,
+    created_at timestamp with time zone not null,
+    updated_at timestamp with time zone not null,
     referrer text not null default ''::text,
     query_params text not null default ''::text,
     duration integer not null default 0,
@@ -28,7 +28,7 @@ create table if not exists
 
 
 create table if not exists
-  public.web_page (
+  public.web_pageview (
     id text not null,
     created_at timestamp without time zone not null default current_timestamp,
     updated_at timestamp without time zone not null,
@@ -36,11 +36,11 @@ create table if not exists
     referrer text not null default ''::text,
     query_params text not null default ''::text,
     duration integer not null default 0,
-    web_session_id text not null,
+    session_id text not null,
     user_id text not null,
     constraint web_page_pkey primary key (id),
-    constraint web_page_user_id_fkey foreign key (user_id) references web_user (id) on update cascade on delete cascade,
-    constraint web_page_web_session_id_fkey foreign key (web_session_id) references web_session (id) on update cascade on delete cascade
+    constraint web_pageview_session_id_fkey foreign key (session_id) references web_session (id) on delete cascade,
+    constraint web_pageview_user_id_fkey foreign key (user_id) references web_user (id) on delete cascade
   ) tablespace pg_default;
 
 create table if not exists
@@ -52,10 +52,10 @@ create table if not exists
     event_name text not null,
     payload text not null default ''::text,
     page_id text not null,
-    web_session_id text not null,
+    session_id text not null,
     user_id text not null,
     constraint web_event_pkey primary key (id),
-    constraint web_event_page_id_fkey foreign key (page_id) references web_page (id) on update cascade on delete cascade,
-    constraint web_event_user_id_fkey foreign key (user_id) references web_user (id) on update cascade on delete cascade,
-    constraint web_event_web_session_id_fkey foreign key (web_session_id) references web_session (id) on update cascade on delete cascade
+    constraint web_event_page_id_fkey foreign key (page_id) references web_pageview (id) on update cascade on delete cascade,
+    constraint web_event_session_id_fkey foreign key (session_id) references web_session (id) on delete cascade,
+    constraint web_event_user_id_fkey foreign key (user_id) references web_user (id) on update cascade on delete cascade
   ) tablespace pg_default;
