d173c91212ff06b7c983a59eb980d324c86642e4
Bereket Engida <bereket@titantech.duckdns.org>
2023-06-11 08:51:51 +0300

    rollback demo dash auth

--- examples/next-js-with-supabase/src/app/api/loglib/route.ts
+++ examples/next-js-with-supabase/src/app/api/loglib/route.ts
@@ -7,7 +7,4 @@ const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SU
 
 export const { POST, GET } = createServerRoutes({
     adapter: supabaseAdapter(supabase),
-    auth: {
-        secret: '123'
-    }
 })
