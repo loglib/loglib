ab53bcc53b61623549d36bc43e6a7bcf04f86598
yonaries <yonaries.dev@gmail.com>
2023-06-15 18:55:46 +0300

    init next-app with nextra-doc

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,7 +1,31 @@
-export default {
-  logo: <span>My Nextra Documentation</span>,
+import React from "react";
+import { DocsThemeConfig, Link } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Loglib</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/LogLib/loglib",
   },
-  // ...
+  chat: {
+    link: "https://t.me/loglib_community",
+  },
+  footer: {
+    text: "MIT 2022 © Loglib",
+  },
+  feedback: {
+    labels: "Feedback from Loglib Docs:",
+    useLink: () => "https://t.me/loglib_community",
+  },
+  editLink: {
+    component: ({ ...props }) => (
+      <Link
+        {...props}
+        href={`https://github.com/LogLib/loglib/apps/docs/${props.filePath}`}
+        target="_blank"
+      />
+    ),
+    text: "Edit this page on GitHub",
+  },
 };
+
+export default config;
