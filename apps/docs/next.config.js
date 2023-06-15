65d15ccbcde7db10b9538e8880fcf4608510c441
yonaries <yonaries.dev@gmail.com>
2023-06-15 16:08:33 +0300

    init next-app with nextra-doc

---
+++ apps/docs/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra();
+
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
