b3e66804a63dafd04848cf89ebe2babdd084c8f4
yonaries <yonaries.dev@gmail.com>
2023-06-16 02:22:22 +0300

    fix: tailwind not working on landing page

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,35 @@
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import type { ReactNode } from "react";
+
+type NextraAppProps = AppProps & {
+  Component: AppProps["Component"] & {
+    getLayout: (page: ReactNode) => ReactNode;
+  };
+};
+
+export default function Nextra({ Component, pageProps }: NextraAppProps) {
+  return (
+    <>
+      {/**
+       * Globally defined svg linear gradient, for use in icons
+       */}
+      <svg height="0px" width="0px">
+        <defs>
+          <linearGradient
+            id="pink-gradient"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(156, 81, 161, 1)" />
+            <stop offset="70%" stopColor="rgba(255, 30, 86, 1)" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <Component {...pageProps} />
+    </>
+  );
+}
