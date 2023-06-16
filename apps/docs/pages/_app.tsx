2ea925b6f4714573caae39ae87e8b58a422cbae0
yonaries <yonaries.dev@gmail.com>
2023-06-16 14:01:57 +0300

    new(site): lot of things added. Icons, patter-background gradient colors and more

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -12,23 +12,6 @@ type NextraAppProps = AppProps & {
 export default function Nextra({ Component, pageProps }: NextraAppProps) {
   return (
     <>
-      {/**
-       * Globally defined svg linear gradient, for use in icons
-       */}
-      <svg height="0px" width="0px">
-        <defs>
-          <linearGradient
-            id="pink-gradient"
-            x1="0%"
-            y1="0%"
-            x2="100%"
-            y2="100%"
-          >
-            <stop offset="0%" stopColor="rgba(156, 81, 161, 1)" />
-            <stop offset="70%" stopColor="rgba(255, 30, 86, 1)" />
-          </linearGradient>
-        </defs>
-      </svg>
       <Component {...pageProps} />
     </>
   );
