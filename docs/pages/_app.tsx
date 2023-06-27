ed13c075c0bd3303b1a015b90b6d8faff686519a
Bereket Engida <bekacru@gmail.com>
2023-06-27 09:20:25 +0300

    add loglib tracker to the docs

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 import type { ReactNode } from "react";
+import Loglib from "@loglib/tracker/react"
 
 type NextraAppProps = AppProps & {
   Component: AppProps["Component"] & {
@@ -13,6 +14,10 @@ export default function Nextra({ Component, pageProps }: NextraAppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Loglib config={{
+        id: "docs_loglib",
+        host: "https://www.loglib.io,"
+      }} />
     </>
   );
 }
