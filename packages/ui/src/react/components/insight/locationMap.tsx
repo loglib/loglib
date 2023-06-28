85cc3b627795a451caa594b8ec1e221a6a7005bd
Bereket Engida <bekacru@gmail.com>
2023-06-28 20:15:56 +0300

    tooltip added

--- packages/ui/src/react/components/insight/locationMap.tsx
+++ packages/ui/src/react/components/insight/locationMap.tsx
@@ -17,8 +17,23 @@ const LocationMap = ({data}: {data:{location: string, visits: number}[]})=>{
     return(
         <div className=" tw-flex tw-justify-center tw-items-center tw-animate-in ">
             {
-                !data.length ? <>
-                </> : 
+                !data.length ? <div className=" flex items-center h-full justify-center relative">
+                    <p className=" text-center animate-pulse bg-gradient-to-tr tw-from-emphasis absolute tw-to-white/80 tw-text-transparent tw-bg-clip-text">
+                        Loading Map...
+                        </p>
+                        <WorldMap
+                        color="#f8a857"
+                        value-suffix="visits"
+                        size="lg"
+                        tooltipBgColor="#000"
+                        richInteraction
+                        borderColor={
+                            theme === "dark" ? "#86868a" : "#222329"
+                        }
+                        backgroundColor="rgba(0, 0, 0, .0)"
+                        data={[]}/>
+              
+                </div> : 
                         <WorldMap
                         color="#f8a857"
                         value-suffix="visits"
--- packages/ui/src/react/dashboard.tsx
+++ packages/ui/src/react/dashboard.tsx
@@ -59,6 +59,7 @@ export const Dashboard: FC<DashboardProps> = (props) => {
         setIsAuth(false)
       }
     },
+    refreshInterval: 1000 * 12
   })
 
   function addFilter(f: Filter) {
@@ -170,12 +171,14 @@ export const Dashboard: FC<DashboardProps> = (props) => {
                         Icon={UserIcon}
                         data={data ? data.insight.uniqueVisitors : { change: 0, total: 0 }}
                         isLoading={isLoading}
+                        tooltip="Unique visitors requesting pages within a specific period, regardless of repeat visits."
                       />
                       <InsightCard
                         title={"Views"}
                         Icon={Eye}
                         data={data ? data.insight.pageView : { change: 0, total: 0 }}
                         isLoading={isLoading}
+                        tooltip="The total number of pages viewed. Repeated views of a single page are counted."
                       />
                       <InsightCard
                         title={"Average Time"}
@@ -183,6 +186,7 @@ export const Dashboard: FC<DashboardProps> = (props) => {
                         data={data ? data.insight.averageTime : { change: 0, total: 0 }}
                         valuePrefix={""}
                         isLoading={isLoading}
+                        tooltip="The average amount of time visitors spend on your website."
                       />
                       <InsightCard
                         title={"Bounce Rate"}
@@ -191,6 +195,7 @@ export const Dashboard: FC<DashboardProps> = (props) => {
                         negative
                         data={data ? data.insight.bounceRate : { change: 0, total: 0 }}
                         isLoading={isLoading}
+                        tooltip=" The percentage of visitors who quickly exit your website without exploring further."
                       />
 
                     </div>
@@ -200,8 +205,8 @@ export const Dashboard: FC<DashboardProps> = (props) => {
                           curTableTab === "locations" ? 
                           <Fragment>
                           <CardHeader className=" tw-flex tw-flex-row tw-gap-2 tw-items-end">
-                          <svg viewBox="0 0 24 24" className=" h-5 w-5 tw-stroke-emphasis" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19.5 6L18.0333 7.1C17.6871 7.35964 17.2661 7.5 16.8333 7.5H13.475C12.8775 7.5 12.3312 7.83761 12.064 8.37206V8.37206C11.7342 9.03161 11.9053 9.83161 12.476 10.2986L14.476 11.9349C16.0499 13.2227 16.8644 15.22 16.6399 17.2412L16.5936 17.6577C16.5314 18.2177 16.4102 18.7695 16.232 19.304L16 20" stroke-width="2"></path> <path d="M2.5 10.5L5.7381 9.96032C7.09174 9.73471 8.26529 10.9083 8.03968 12.2619L7.90517 13.069C7.66434 14.514 8.3941 15.9471 9.70437 16.6022V16.6022C10.7535 17.1268 11.2976 18.3097 11.0131 19.4476L10.5 21.5" className=" tw-bg-emphasis" stroke-width="2"></path> <circle cx="12" cy="12" r="9" className=" tw-stroke-emphasis" stroke-width="2"></circle> </g></svg>
-                              <CardTitle className="tw-text-base">Visitors Map</CardTitle>
+                          {/* <svg viewBox="0 0 24 24" className=" h-5 w-5 tw-stroke-emphasis" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19.5 6L18.0333 7.1C17.6871 7.35964 17.2661 7.5 16.8333 7.5H13.475C12.8775 7.5 12.3312 7.83761 12.064 8.37206V8.37206C11.7342 9.03161 11.9053 9.83161 12.476 10.2986L14.476 11.9349C16.0499 13.2227 16.8644 15.22 16.6399 17.2412L16.5936 17.6577C16.5314 18.2177 16.4102 18.7695 16.232 19.304L16 20" stroke-width="2"></path> <path d="M2.5 10.5L5.7381 9.96032C7.09174 9.73471 8.26529 10.9083 8.03968 12.2619L7.90517 13.069C7.66434 14.514 8.3941 15.9471 9.70437 16.6022V16.6022C10.7535 17.1268 11.2976 18.3097 11.0131 19.4476L10.5 21.5" className=" tw-bg-emphasis" stroke-width="2"></path> <circle cx="12" cy="12" r="9" className=" tw-stroke-emphasis" stroke-width="2"></circle> </g></svg> */}
+                              <CardTitle className="tw-text-base py-4">Visitors Map</CardTitle>
 
                           </CardHeader> 
                           <CardContent className={
