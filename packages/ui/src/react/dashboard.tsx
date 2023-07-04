306b1e6306d3174bf59d9dfa060eca47af2b3fc5
Bereket Engida <bekacru@gmail.com>
2023-07-04 17:44:01 +0300

    fix(ui): scroll fix

--- packages/ui/src/react/dashboard.tsx
+++ packages/ui/src/react/dashboard.tsx
@@ -121,7 +121,7 @@ export const Dashboard: FC<DashboardProps> = (props = {
       theme === "dark" ? "dark" : ""
     }>
       <LayoutGroup>
-        <div style={props.style} className={cn("tw-min-h-screen dark:tw-bg-[#02060f] tw-bg-white tw-w-full tw-space-y-4 tw-p-8 tw-pt-6 tw-transition-all tw-duration-700 dark:tw-text-white/80 scrollbar-hide", props.className)}>
+        <div style={props.style} className={cn(" dark:tw-bg-[#02060f] tw-bg-white tw-w-full tw-space-y-4 tw-p-8 tw-pt-6 tw-transition-all tw-duration-700 dark:tw-text-white/80 scrollbar-hide", props.className)}>
           <props.components.header timezone={timezone} setTimezone={setTimezone} timezones={timezones} logoutFn={defaultLogout} hideLogout={!token} />
           {isAuth || props.noAuth ?
             <Tabs defaultValue="insights" className="tw-space-y-4" >
@@ -237,7 +237,6 @@ export const Dashboard: FC<DashboardProps> = (props = {
                           curTableTab === "locations" ?
                             <Fragment>
                               <CardHeader className=" tw-flex tw-flex-row tw-gap-2 tw-items-end">
-                                {/* <svg viewBox="0 0 24 24" className=" h-5 w-5 tw-stroke-emphasis" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19.5 6L18.0333 7.1C17.6871 7.35964 17.2661 7.5 16.8333 7.5H13.475C12.8775 7.5 12.3312 7.83761 12.064 8.37206V8.37206C11.7342 9.03161 11.9053 9.83161 12.476 10.2986L14.476 11.9349C16.0499 13.2227 16.8644 15.22 16.6399 17.2412L16.5936 17.6577C16.5314 18.2177 16.4102 18.7695 16.232 19.304L16 20" stroke-width="2"></path> <path d="M2.5 10.5L5.7381 9.96032C7.09174 9.73471 8.26529 10.9083 8.03968 12.2619L7.90517 13.069C7.66434 14.514 8.3941 15.9471 9.70437 16.6022V16.6022C10.7535 17.1268 11.2976 18.3097 11.0131 19.4476L10.5 21.5" className=" tw-bg-emphasis" stroke-width="2"></path> <circle cx="12" cy="12" r="9" className=" tw-stroke-emphasis" stroke-width="2"></circle> </g></svg> */}
                                 <CardTitle className="tw-text-base py-4">Visitors Map</CardTitle>
 
                               </CardHeader>
