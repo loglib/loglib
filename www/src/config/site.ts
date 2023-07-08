c18005b8fdf4f2bd2d07405cd8dc1f6ba21c2837
Bereket Engida <bekacru@gmail.com>
2023-07-09 01:22:29 +0300

    fix: og image

--- www/src/config/site.ts
+++ www/src/config/site.ts
@@ -2,8 +2,7 @@ import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
   name: "LOGLIB",
-  description:
-    "Yet another web analytics platform.",
+  description: "Privacy first. Open source. Web analytics.",
   url: "https://www.loglib.io",
   ogImage: "https://www.loglib.io/og.png",
   links: {
