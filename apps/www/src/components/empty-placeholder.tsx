e6b86fba08ff5bf99f31330e6fb98d7f0f9c92be
bekacru <bekacru@gmail.com>
2023-08-10 13:34:38 +0300

    fix: typecheck

--- apps/www/src/components/empty-placeholder.tsx
+++ apps/www/src/components/empty-placeholder.tsx
@@ -39,6 +39,7 @@ EmptyPlaceholder.Icon = function EmptyPlaceHolderIcon({
     }
     return (
         <div className="bg-muted flex h-20 w-20 items-center justify-center rounded-full">
+            {/* @ts-ignore */}
             <Icon className={cn("h-10 w-10", className)} {...props} />
         </div>
     );
