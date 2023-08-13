bcdb58c8d491652e9b5a14064979e4af9d50785e
bekacru <bekacru@gmail.com>
2023-08-13 14:35:22 +0300

    fix: dev ip address

--- apps/api/src/lib/detect/get-ip-address.ts
+++ apps/api/src/lib/detect/get-ip-address.ts
@@ -2,10 +2,6 @@ import requestIp from "request-ip";
 import { ApiRequest } from "../../routes/type";
 
 export function getIpAddress(req: ApiRequest) {
-    // manual override
-    if (process.env.CLIENT_IP_ADDRESS as string) {
-        return process.env.CLIENT_IP_ADDRESS;
-    }
     if (process.env.NODE_ENV === "development") return "";
     // Cloudflare
     else if (req.headers["cf-connecting-ip"]) {
