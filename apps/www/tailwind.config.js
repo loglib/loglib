f7139e0e54f887e03a22a769808af7cef10bcdf8
bekacru <bekacru@gmail.com>
2023-08-17 20:42:53 +0300

    fix: removed tremor

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -148,31 +148,10 @@ module.exports = {
                 "background-pan": "background-pan 3s linear infinite",
                 "spin-slow": "spin-slow 10s linear infinite",
             },
-            boxShadow: {
-                // light
-                "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-                "tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-                "tremor-dropdown":
-                    "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-                // dark
-                "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-                "dark-tremor-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-                "dark-tremor-dropdown":
-                    "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-            },
             borderRadius: {
-                "tremor-small": "0.375rem",
-                "tremor-default": "0.5rem",
-                "tremor-full": "9999px",
                 lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
                 sm: "calc(var(--radius) - 4px)",
-            },
-            fontSize: {
-                "tremor-label": ["0.75rem"],
-                "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
-                "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
-                "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
             },
         },
     },
