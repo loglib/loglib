9e290017a3d97883e775ee1952930a1dfa30b0de
bekacru <bekacru@gmail.com>
2023-09-02 21:17:45 +0300

    fix: ts fix on url env

--- apps/www/src/lib/db.ts
+++ apps/www/src/lib/db.ts
@@ -5,7 +5,12 @@ import { env } from 'env.mjs';
 
 
 export const getDbUrl = () => {
-    if (process.env.NODE_ENV === "production" || env.DATABASE_AUTH_TOKEN) return env.DATABASE_URL
+    if (process.env.NODE_ENV === "production" || env.DATABASE_AUTH_TOKEN) {
+        if (!env.DATABASE_URL) {
+            throw Error("❌ DATABASE URL MISSING")
+        }
+        return env.DATABASE_URL
+    }
     const workDir = process.cwd()
     const dir = workDir.split("/")
     const dbPath = `file:${dir.slice(0, dir.length - 2).join("/")}/packages/db/db.sqlite`
