31c608821409da8111b4226fa51d06b13ffdcdfb
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2023-09-08 00:05:23 +0300

    fix: yet another one

--- apps/www/src/app/(landing)/page.tsx
+++ apps/www/src/app/(landing)/page.tsx
@@ -1,5 +1,5 @@
+
 import Changelog from "@/components/changelog";
-import ContributorsAvatar from "@/components/contributors";
 import { TwitterXIcons } from "@/components/extra-icons";
 
 import { FeatureAccordion } from "@/components/landing/feature-accordion";
@@ -10,6 +10,22 @@ import { RepurposeYourData } from "@/components/landing/repurpose-your-data";
 import { Community } from "@/components/marketing/community";
 import { TrackView } from "@loglib/tracker/react";
 import Link from "next/link";
+
+async function getPackageInfo(packageName: string) {
+  try {
+    const response = await fetch(`https://registry.npmjs.org/${packageName}`);
+    const packageData = await response.json();
+    const { version, dist } = packageData.versions[packageData['dist-tags'].latest];
+    const packageSize = dist.unpackedSize;
+    return {
+      version,
+      packageSize: Math.floor(packageSize / 1024)
+    }
+  } catch (error) {
+    console.error(`Error fetching package info: ${error}`);
+  }
+}
+
 
 async function getGitHubStars() {
   try {
@@ -28,9 +44,9 @@ async function getGitHubStars() {
     return null;
   }
 }
-async function getGitHubContributors() {
+async function getGitHubForks() {
   return await fetch(
-    "https://api.github.com/repos/loglib/loglib/contributors",
+    "https://api.github.com/repos/loglib/loglib/forks",
     {
       method: "GET",
       redirect: "follow",
@@ -46,7 +62,6 @@ async function getGitHubContributors() {
 
 export default async function IndexPage() {
   const stars = await getGitHubStars();
-  const contributors = await getGitHubContributors();
   const forks = await getGitHubForks();
   const npm = await getPackageInfo("@loglib/tracker")
   return (
@@ -54,18 +69,17 @@ export default async function IndexPage() {
       <HeroSection />
       <FeatureCard />
       <FeatureAccordion />
-      <div className="hidden md:block">
-        <ProjectsContents gitForks={forks} githubStars={stars ? parseInt(stars) : 100} npmSize={npm?.packageSize ?? 1} npmVersion={npm?.version ?? "0.6.2"} />
-      </div>
       <section className="w-full">
         <RepurposeYourData />
+
         <section className="w-full">
           <Changelog />
         </section>
       </section>
 
-      
-
+      <div className="hidden md:block">
+        <ProjectsContents gitForks={forks} githubStars={stars ? parseInt(stars) : 100} npmSize={npm?.packageSize ?? 1} npmVersion={npm?.version ?? "0.6.2"} />
+      </div>
       <section className="max-w-8xl to-50 flex-col mx-auto mt-10 w-full rounded-3xl flex  md:flex-row md:justify-between justify-center md:items-start items-center  bg-gradient-to-br from-gray-100 px-4 dark:from-stone-950/80 dark:to-[#080812] sm:px-16">
         <div className="flex h-min px-2  flex-col justify-center gap-8 py-12">
           <h1 className="font-heading max-w-3xl text-3xl font-bold sm:text-6xl">
@@ -79,10 +93,9 @@ export default async function IndexPage() {
         </div>
 
       </section>
-
       <section className="flex flex-col justify-center items-center gap-4">
-        <p>Made Possible By</p>
-        <ContributorsAvatar />
+        <p>Made Possible By</p>         
+        <ContributorsAvatar />      
       </section>
       <TrackView
         name="footer-reached"
@@ -107,7 +120,6 @@ export default async function IndexPage() {
           <div className=" flex items-center gap-2">
             <Link href="https://twiiter.com/loglib_io">
               <TwitterXIcons width={15} height={15} size={15} />
-             
             </Link>
             <Link href="https://github.com/loglib">
               <svg
@@ -160,11 +172,10 @@ export default async function IndexPage() {
                   strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
-                  {" "}
                   <path
                     d="M18.59 5.88997C17.36 5.31997 16.05 4.89997 14.67 4.65997C14.5 4.95997 14.3 5.36997 14.17 5.69997C12.71 5.47997 11.26 5.47997 9.83001 5.69997C9.69001 5.36997 9.49001 4.95997 9.32001 4.65997C7.94001 4.89997 6.63001 5.31997 5.40001 5.88997C2.92001 9.62997 2.25001 13.28 2.58001 16.87C4.23001 18.1 5.82001 18.84 7.39001 19.33C7.78001 18.8 8.12001 18.23 8.42001 17.64C7.85001 17.43 7.31001 17.16 6.80001 16.85C6.94001 16.75 7.07001 16.64 7.20001 16.54C10.33 18 13.72 18 16.81 16.54C16.94 16.65 17.07 16.75 17.21 16.85C16.7 17.16 16.15 17.42 15.59 17.64C15.89 18.23 16.23 18.8 16.62 19.33C18.19 18.84 19.79 18.1 21.43 16.87C21.82 12.7 20.76 9.08997 18.61 5.88997H18.59ZM8.84001 14.67C7.90001 14.67 7.13001 13.8 7.13001 12.73C7.13001 11.66 7.88001 10.79 8.84001 10.79C9.80001 10.79 10.56 11.66 10.55 12.73C10.55 13.79 9.80001 14.67 8.84001 14.67ZM15.15 14.67C14.21 14.67 13.44 13.8 13.44 12.73C13.44 11.66 14.19 10.79 15.15 10.79C16.11 10.79 16.87 11.66 16.86 12.73C16.86 13.79 16.11 14.67 15.15 14.67Z"
                     fill="#fff"
-                  ></path>{" "}
+                  ></path>
                 </g>
               </svg>
             </Link>
