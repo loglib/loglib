8c220bd33bfb5f5e407b024d3918f176282f4011
bekacru <bekacru@gmail.com>
2023-09-29 13:35:50 +0300

    fixed some mobile nav issues (#142)
    * fixed some mobile nav issues
    
    * fixed some bug
    
    * fixed a bug
    
    * removed an overflow
    
    * made a quick fix

--- apps/www/src/components/landing-nav.tsx
+++ apps/www/src/components/landing-nav.tsx
@@ -5,33 +5,18 @@ import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
-export const LandingNav = () => {
+export const LandingNav = ({ mobileView }: { mobileView?: boolean }) => {
   const pathname = usePathname();
-  return (
-    <div className=" sticky top-1 overflow-hidden rounded-xl [--duration:500ms]  [transform:translateZ(0)]">
-      <nav className="relative w-full rounded-xl border border-black/10 p-2 dark:border-white/10">
-        <div
-          className={cn(
-            "dark:bg-logo/95 absolute bottom-0 left-0 h-1/2 w-[var(--width)] translate-x-2 bg-black/75 blur-xl transition-[width,transform] duration-500",
-            pathname === "/" && "translate-x-2",
-            pathname === "/pricing" && "translate-x-20",
-            // pathname === "/blog" && "translate-x-44",
-            pathname?.includes("/blog") && "translate-x-44",
-            pathname?.includes("/changelog") && "translate-x-72",
-            pathname?.includes("/docs") && " translate-x-96"
-          )}
-          style={{
-            width: "84.5px",
-            left: "0px",
-          }}
-        ></div>
-        <div className="absolute inset-0 rounded-xl bg-gradient-to-tr backdrop-blur-md dark:from-stone-950  dark:to-stone-950/80"></div>
-        <div className="relative">
+  if (!mobileView) {
+    return (
+      <div className=" sticky top-1 overflow-hidden rounded-xl [--duration:500ms]  [transform:translateZ(0)]">
+        <nav className="relative w-full rounded-xl border border-black/10 p-2 dark:border-white/10">
           <div
             className={cn(
-              "bg-black/39 dark:bg-logo/30 absolute bottom-0 left-0 h-1/3 w-7 translate-x-2  rounded-full blur-md transition-[width,transform] duration-500",
+              "dark:bg-logo/95 absolute bottom-0 left-0 h-1/2 w-[var(--width)] translate-x-2 bg-black/75 blur-xl transition-[width,transform] duration-500",
               pathname === "/" && "translate-x-2",
               pathname === "/pricing" && "translate-x-20",
+              // pathname === "/blog" && "translate-x-44",
               pathname?.includes("/blog") && "translate-x-44",
               pathname?.includes("/changelog") && "translate-x-72",
               pathname?.includes("/docs") && " translate-x-96"
@@ -41,82 +26,169 @@ export const LandingNav = () => {
               left: "0px",
             }}
           ></div>
-          <ul className="relative flex items-center gap-4">
-            <li className="">
-              <Link href="/">
-                <button
-                  type="button"
-                  className={cn(
-                    "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
-                    pathname === "/" && "text-black/80 dark:text-white/75"
-                  )}
-                >
-                  Home
-                </button>
-              </Link>
-            </li>
+          <div className="absolute inset-0 rounded-xl bg-gradient-to-tr backdrop-blur-md dark:from-stone-950  dark:to-stone-950/80"></div>
+          <div className="relative">
+            <div
+              className={cn(
+                "bg-black/39 dark:bg-logo/30 absolute bottom-0 left-0 h-1/3 w-7 translate-x-2  rounded-full blur-md transition-[width,transform] duration-500",
+                pathname === "/" && "translate-x-2",
+                pathname === "/pricing" && "translate-x-20",
+                pathname?.includes("/blog") && "translate-x-44",
+                pathname?.includes("/changelog") && "translate-x-72",
+                pathname?.includes("/docs") && " translate-x-96"
+              )}
+              style={{
+                width: "84.5px",
+                left: "0px",
+              }}
+            ></div>
+            <ul className="relative flex items-center gap-4">
+              <li className="">
+                <Link href="/">
+                  <button
+                    type="button"
+                    className={cn(
+                      "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                      pathname === "/" && "text-black/80 dark:text-white/75"
+                    )}
+                  >
+                    Home
+                  </button>
+                </Link>
+              </li>
 
-            <li className="">
-              <Link href="/pricing">
-                <button
-                  type="button"
-                  className={cn(
-                    "text-black/6 0 px-4 py-1 text-sm font-light transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
-                    pathname === "/pricing" &&
+              <li className="">
+                <Link href="/pricing">
+                  <button
+                    type="button"
+                    className={cn(
+                      "text-black/6 0 px-4 py-1 text-sm font-light transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                      pathname === "/pricing" &&
                       "text-black/80 dark:text-white/75"
-                  )}
-                >
-                  Pricing
-                </button>
-              </Link>
-            </li>
-            <li className="hidden md:block">
-              <Link href="/blog">
-                <button
-                  type="button"
-                  className={cn(
-                    "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
-                    pathname?.includes("/blog") &&
+                    )}
+                  >
+                    Pricing
+                  </button>
+                </Link>
+              </li>
+              <li className="hidden md:block">
+                <Link href="/blog">
+                  <button
+                    type="button"
+                    className={cn(
+                      "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                      pathname?.includes("/blog") &&
                       "text-black/80 dark:text-white/75"
-                  )}
-                >
-                  Blog
-                </button>
-              </Link>
-            </li>
+                    )}
+                  >
+                    Blog
+                  </button>
+                </Link>
+              </li>
 
-            <li className="hidden md:block">
-              <Link href="/changelog">
-                <button
-                  type="button"
-                  className={cn(
-                    "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
-                    pathname?.includes("/docs") &&
+              <li className="hidden md:block">
+                <Link href="/changelog">
+                  <button
+                    type="button"
+                    className={cn(
+                      "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                      pathname?.includes("/docs") &&
                       "text-black/80 dark:text-white/75"
-                  )}
-                >
-                  Changelog
-                </button>
-              </Link>
-            </li>
+                    )}
+                  >
+                    Changelog
+                  </button>
+                </Link>
+              </li>
 
-            <li className="hidden md:block">
-              <Link href="/docs">
-                <button
-                  type="button"
-                  className={cn(
-                    "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
-                    pathname?.includes("/docs") &&
+              <li className="hidden md:block">
+                <Link href="/docs">
+                  <button
+                    type="button"
+                    className={cn(
+                      "px-4 py-1 text-sm font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                      pathname?.includes("/docs") &&
                       "text-black/80 dark:text-white/75"
-                  )}
-                >
-                  Docs
-                </button>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+                    )}
+                  >
+                    Docs
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+  else {
+    return (
+      <ul className="relative flex flex-col gap-6 pl-[1.5rem] pt-[10rem]">
+        <li className="">
+          <Link href="/">
+            <button
+              type="button"
+              className={cn(
+                "px-4 py-1 text-lg font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                pathname === "/" && "text-black/80 dark:text-white/75 font-bold",
+              )}
+            >
+              Home
+            </button>
+          </Link>
+        </li>
+
+        <li className="">
+          <Link href="/pricing">
+            <button
+              type="button"
+              className={cn(
+                "text-black/6 0 px-4 py-1 text-lg font-light transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                pathname === "/pricing" &&
+                "text-black/80 dark:text-white/75 font-bold",
+              )}
+            >
+              Pricing
+            </button>
+          </Link>
+        </li>
+
+        <li className="">
+          <Link href="/changelog">
+            <button
+              type="button"
+              className={cn(
+                "px-4 py-1 text-lg font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                pathname?.includes("/docs") &&
+                "text-black/80 dark:text-white/75 font-bold",
+              )}
+            >
+              Changelog
+            </button>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/docs">
+            <button
+              type="button"
+              className={cn(
+                "px-4 py-1 text-lg font-light text-black/60 transition-[text-shadow,color] duration-200 hover:text-black/80 focus:outline-none dark:text-white/60 dark:hover:text-white/75",
+                pathname?.includes("/docs") &&
+                "text-black/80 dark:text-white/75 font-bold",
+              )}
+            >
+              Docs
+            </button>
+          </Link>
+        </li>
+      </ul>
+
+    )
+  }
+
 };
+{
+  /**
+   * flex items-center justify-between md:mx-16 sticky top-0 mb-16 z-50 bg-white/60 backdrop-blur-sm dark:bg-stone-950
+   */
+}
