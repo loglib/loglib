219aef064858293ac8f34a36b8ba675df45c8eb3
Bereket Engida <bekacru@gmail.com>
2024-08-16 12:08:07 +0300

    feat: disable usage check

--- apps/api/src/lib/check-usage.ts
+++ apps/api/src/lib/check-usage.ts
@@ -1,6 +1,11 @@
 import { db } from "../db/drizzle";
 
+const disableCheck = process.env.DISABLE_USAGE_CHECK === "true";
+
 export const checkUsage = async (websiteId: string) => {
+    if (disableCheck) {
+        return true;
+    }
     const sentEmails = await db.query.sentEmail.findFirst({
         where(fields, operators) {
             return operators.eq(fields.websiteId, websiteId);
